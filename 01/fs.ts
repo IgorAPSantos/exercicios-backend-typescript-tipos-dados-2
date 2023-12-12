b19c943cd30b3c37a3cf554ba7302c219df5a034
IgorAPSantos <igorppandre@hotmail.com>
2023-12-12 18:00:10 -0300

    utilizando o fs sincrono

--- 01/fs.ts
+++ 01/fs.ts
@@ -1,38 +1,22 @@
-const fs = require('fs/promises')
+const fs = require('fs')
 
-const lerArquivo = async (): Promise<unknown> => {
-    try {
-        return JSON.parse(await fs.readFile('./bd.json'))
-
-    } catch (error) {
-        if (
-            error !== null &&
-            typeof error === 'object' &&
-            'message' in error &&
-            typeof error.message === 'string'
-        ) {
-            return console.log(error.message);
-        }
-    }
+const lerArquivo = (): unknown => {
+    return JSON.parse(fs.readFileSync('../bd.json'))
 }
 
-const escreverArquivo = async (conteudo: any): Promise<void> => {
-    try {
-        conteudo = JSON.stringify(conteudo)
-        await fs.writeFile('./bd.json', conteudo)
-
-    } catch (error) {
-        if (
-            error !== null &&
-            typeof error === 'object' &&
-            'message' in error &&
-            typeof error.message === 'string'
-        ) {
-            console.log(error.message);
-        }
-    }
-
+const escreverArquivo = (conteudo: any): void => {
+    fs.writeFileSync('../bd.json', JSON.stringify(conteudo))
 }
+
+// const fs = require('fs/promises')
+
+// const lerArquivo = async (): Promise<unknown> => {
+//     return JSON.parse(await fs.readFile('../bd.json'))
+// }
+
+// const escreverArquivo = async (conteudo: any): Promise<void> => {
+//     await fs.writeFile('../bd.json', JSON.stringify(conteudo))
+// }
 
 module.exports = {
     lerArquivo,
